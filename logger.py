6a32957217a099665734c9035a3cf50659d2ad39
Zain Malik <zainmalik7345@gmail.com>
2024-03-18 16:29:47 +0500

    FEAT: logger

---
+++ logger.py
@@ -0,0 +1,45 @@
+import logging
+from colorlog import ColoredFormatter
+
+
+class BaseLogger(logging.getLogger(__name__).__class__):
+    """
+    Override this class for more customizations.
+    """
+    LOGGER_FORMAT = ("  %(log_color)s%(levelname)-8s%(reset)s | %(log_color)s"
+                     " {%(filename)s:%(lineno)s - %(funcName)s} %(message)s%(reset)s")
+
+    def __init__(self, name, level=logging.DEBUG):
+        super().__init__(name, level)
+
+        stream = logging.StreamHandler()
+        stream.setFormatter(ColoredFormatter(self.LOGGER_FORMAT))
+
+        self.addHandler(stream)
+        self.setLevel(level)
+
+    def addHandler(self, hdlr=None):
+        """
+        Not used anymore
+        :param hdlr:
+        :return:
+        """
+        return super().addHandler(hdlr)
+
+    def setLevel(self, level):
+        """
+        not used anymore
+        :param level:
+        :return:
+        """
+        return super().setLevel(level)
+
+
+logger = BaseLogger(__name__)
+
+if __name__ == '__main__':
+    logger.debug("A quirky message only developers care about")
+    logger.info("Curious users might want to know this")
+    logger.warning("Something is wrong and any user should be informed")
+    logger.error("Serious stuff, this is red for a reason")
+    logger.critical("OH NO everything is on fire")
